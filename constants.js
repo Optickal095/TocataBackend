01c8fc5310e5d38619a53dd968aae6387d6b6873
Optickal <eduardo.he095@gmail.com>
2023-05-07 17:39:44 -0400

    JWT

--- constants.js
+++ constants.js
@@ -5,10 +5,13 @@ const DB_HOST = "tocata.jmeuact.mongodb.net";
 const API_VERSION = "v1";
 const IP_SERVER = "localhost";
 
+const JWT_SECRET_KEY = "LYYWbTfkDK7IqmxmCLGEEDSC8yUrX-GKuU2dcEiYl";
+
 module.exports = {
   DB_USER,
   DB_PASSWORD,
   DB_HOST,
   API_VERSION,
-  IP_SERVER
+  IP_SERVER,
+  JWT_SECRET_KEY
 };
