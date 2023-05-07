93f08fff2500eb02877bbc1be398487e18411f9c
Optickal <eduardo.he095@gmail.com>
2023-05-07 16:51:50 -0400

    Api

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const { API_VERSION } = require("./constants");
+
+const app = express();
+
+// Import Routing
+
+// Configure Body Parser
+
+// Configure Header HTTP - CORS
+
+//  Configure Routing
+
+module.exports = app;
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const app = require("./app");
 const {
   DB_USER,
   DB_PASSWORD,
@@ -7,11 +8,18 @@ const {
   API_VERSION
 } = require("./constants");
 
+const PORT = process.env.POST || 3977;
+
 mongoose.connect(
   `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/`,
   (error) => {
     if (error) throw error;
 
-    console.log("La conexion con la base de datos ha sido exitosa");
+    app.listen(PORT, () => {
+      console.log("##################");
+      console.log("#### API REST ####");
+      console.log("##################");
+      console.log(`http://${IP_SERVER}:${PORT}/api/${API_VERSION}/`);
+    });
   }
 );
