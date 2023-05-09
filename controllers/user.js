38b7136a114c8b48793304c6d64ebe25ae227f60
Optickal <eduardo.he095@gmail.com>
2023-05-09 16:59:09 -0400

    updateUser

--- controllers/user.js
+++ controllers/user.js
@@ -2,18 +2,20 @@ const bcrypt = require("bcryptjs");
 const User = require("../models/user");
 const image = require("../utils/image");
 
+// getMe Function
 async function getMe(req, res) {
   const { user_id } = req.user;
 
   const response = await User.findById(user_id);
 
   if (!response) {
-    res.status(400).send({ msg: "Nose ha encontrado usuario" });
+    res.status(400).send({ msg: "No se ha encontrado usuario" });
   } else {
     res.status(200).send(response);
   }
 }
 
+// getUsers Function
 async function getUsers(req, res) {
   const { active } = req.query;
   let response = null;
@@ -27,19 +29,23 @@ async function getUsers(req, res) {
   res.status(200).send(response);
 }
 
+// createUser Function
 async function createUser(req, res) {
   const { password } = req.body;
   const user = new User({ ...req.body, active: false });
 
+  // Crypting Password
   const salt = bcrypt.genSaltSync(10);
   const hashPassword = bcrypt.hashSync(password, salt);
   user.password = hashPassword;
 
+  // Image
   if (req.files.avatar) {
     const imagePath = image.getFilePath(req.files.avatar);
     user.avatar = imagePath;
   }
 
+  // Save User
   user.save((error, userStorage) => {
     if (error) {
       res.status(400).send({ msg: "Error al crear el ususario" });
@@ -49,8 +55,39 @@ async function createUser(req, res) {
   });
 }
 
+//updateUser Function
+async function updateUser(req, res) {
+  const { id } = req.params;
+  const userData = req.body;
+
+  // Crypting Password
+  if (userData.password) {
+    const salt = bcrypt.genSaltSync(10);
+    const hashPassword = bcrypt.hashSync(userData.password, salt);
+    userData.password = hashPassword;
+  } else {
+    delete userData.password;
+  }
+
+  // Image
+  if (req.files.avatar) {
+    const imagePath = image.getFilePath(req.files.avatar);
+    userData.avatar = imagePath;
+  }
+
+  // Update and Save User
+  User.findByIdAndUpdate({ _id: id }, userData, (error) => {
+    if (error) {
+      res.status(400).send({ msg: "Error al actualizar el ususario" });
+    } else {
+      res.status(200).send({ msg: "Actualización correcta" });
+    }
+  });
+}
+
 module.exports = {
   getMe,
   getUsers,
-  createUser
+  createUser,
+  updateUser
 };
