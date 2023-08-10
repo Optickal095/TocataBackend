b104b94ef4e019e1ea3a75b3a486cfb0efb19351
Eduardo Hernández Oyarzún <eduardo.he095@gmail.com>
2023-08-10 02:36:57 -0400

    imagePath fix

--- utils/image.js
+++ utils/image.js
@@ -1,6 +1,6 @@
 function getFilePath(file) {
   const filePath = file.path;
-  const fileSplit = filePath.split("\\");
+  const fileSplit = filePath.split("/");
 
   return `${fileSplit[1]}/${fileSplit[2]}`;
 }
